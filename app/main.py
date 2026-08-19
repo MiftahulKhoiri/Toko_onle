ccae84ab1ae8cc603a8a1108cfe200581be30a46
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-08-19 18:30:32 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,25 +1,19 @@
 # app/main.py
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 
 from app import models
 from app.database import engine
-from app.routers import produk, auth, keranjang, payment
+from app.routers import produk, auth, keranjang, payment, pages
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Toko Salome Cakyud")
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
-templates = Jinja2Templates(directory="app/templates")
 
+app.include_router(pages.router)
 app.include_router(produk.router)
 app.include_router(auth.router)
 app.include_router(keranjang.router)
 app.include_router(payment.router)
-
-
-@app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "nama_toko": "Salome Cakyud"})
