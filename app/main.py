114d78925d0cd3e87a5b49c0b249b7dcd9a4fd26
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-08-19 18:08:55 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from fastapi.templating import Jinja2Templates
 
 from app import models
 from app.database import engine
-from app.routers import produk, auth
+from app.routers import produk, auth, keranjang
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -16,6 +16,7 @@ templates = Jinja2Templates(directory="app/templates")
 
 app.include_router(produk.router)
 app.include_router(auth.router)
+app.include_router(keranjang.router)
 
 
 @app.get("/")
