0febde76ddcd607f3d9450c40a9ed7a4f5599745
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-08-19 18:59:04 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# app/main.py
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from app import models
+from app.database import engine
+from app.routers import produk, auth, keranjang, payment, pages
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Toko Salome Cakyud")
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+app.include_router(pages.router)
+app.include_router(produk.router)
+app.include_router(auth.router)
+app.include_router(keranjang.router)
+app.include_router(payment.router)
