36675bd1b6355100a66b1e37fa2357b96f080168
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-08-20 00:05:14 +0800

    Update dependencies.py

--- app/dependencies.py
+++ app/dependencies.py
@@ -33,3 +33,9 @@ def get_current_user(
         raise credentials_exception
 
     return user
+
+
+def get_current_admin(current_user: models.User = Depends(get_current_user)) -> models.User:
+    if not current_user.is_admin:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Khusus admin")
+    return current_user
