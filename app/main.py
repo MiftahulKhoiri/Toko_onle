a70a75c239d7626c436ee503ca3e0ab282f2402d
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2026-08-19 18:00:40 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from fastapi.templating import Jinja2Templates
 
 from app import models
 from app.database import engine
+from app.routers import produk
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -12,6 +13,9 @@ app = FastAPI(title="Toko Salome Cakyud")
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
+
+app.include_router(produk.router)
+
 
 @app.get("/")
 def home(request: Request):
